f36a394cc5f89eefc7a1f04932d8508dbc3f79f8
VladLi742 <electro_stalin@yahoo.com>
2017-08-14 15:30:34 +0300

    Вывод данных на таблицу посредством getJSON
    Используя публичный json api для получения курсов валют, вывел на страницу информацию о валютах в виде таблицы (поля: вид валюты, курс покупки в RUB, курс продажи в RUB) и дату со временем последнего обновления данных

---
+++ first/js/main.js
@@ -0,0 +1,13 @@
+$.getJSON('https://query.yahooapis.com/v1/public/yql?q=select+%2A+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',function(data) {
+    var items = [];
+    $(data.query.results.rate).each(function(key,val) {
+      items.push("<tr>");
+      items.push("<td id='" + key + "'>" + val.Name + "</td>");
+      items.push("<td id='" + key + "'>" + val.Ask + "</td>");
+      items.push("<td id='" + key + "'>" + val.Bid + "</td>");
+      items.push("<td id='" + key + "'>" + val.Date + "</td>");
+      items.push("<td id='" + key + "'>" + val.Time + "</td>");
+      items.push("</tr>");
+    });
+    $("<tbody/>", {html: items.join("")}).appendTo("table");
+  });
